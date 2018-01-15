341329eb7feb6772e867c1d3f40b2a694b51669d
user <greenteamer@bk.ru>
2018-01-15 17:12:35 +0300

    eslint added

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
 
 const instructions = Platform.select({
@@ -19,8 +19,31 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const gray = '#F5FCFF';
+const gray2 = '#333333';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: gray,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: gray2,
+    marginBottom: 5,
+  },
+});
+
 export default class App extends Component<{}> {
   render() {
+    this.foo();
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -37,21 +60,3 @@ export default class App extends Component<{}> {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
